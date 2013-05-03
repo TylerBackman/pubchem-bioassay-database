6a37011d9c6fe348626d1d804d264d70c0efb8f8
TylerBackman <nospam@nospam.nospam>
2013-05-03 15:43:32 -0700

    draft of database creator

--- scripts/buildBioassayDatabase.R
+++ scripts/buildBioassayDatabase.R
@@ -1,3 +1,98 @@
 #!/usr/bin/env Rscript
 
-1
+# (C) 2013 Tyler WH Backman
+# Purpose: build a bioassay SQLite database from a downloaded mirror
+
+library(R.utils)
+library(RSQLite)
+library(foreach)
+library(doParallel)
+
+# setup multiprocessing options
+cl <- makeCluster(8)
+registerDoParallel(cl)
+
+bioassayMirror = commandArgs(trailingOnly=TRUE)[1]
+outputDatabase = commandArgs(trailingOnly=TRUE)[2]
+
+bioassayMirror <- "working/bioassayMirror_test" # TEST CODE
+outputDatabase <- "working/bioassayDatabase_test.sqlite" # TEST CODE
+
+# this function returns the path of each assay file within a given folder name
+getAssayPaths <- function(path) {
+    dirs <- list.dirs(path)
+    # regex for integer_integer path
+    assaydirs <- grep("\\d{7}_\\d{7}$", dirs, perl = TRUE, value = TRUE)
+    list.files(assaydirs, full.names = TRUE)
+}
+
+# create database and connect to it
+drv <- dbDriver("SQLite")
+con <- dbConnect(drv, dbname=outputDatabase)
+dbGetQuery(con, "CREATE TABLE sources (source_id INTEGER PRIMARY KEY ASC, description TEXT, version TEXT)")
+dbGetQuery(con, "CREATE TABLE assays (source_id INTEGER, aid INTEGER, targets TEXT, target_type TEXT, assay_type TEXT, organism TEXT)")
+dbGetQuery(con, "CREATE TABLE activity (aid INTEGER, sid INTEGER, cid INTEGER, activity INTEGER, score INTEGER)")
+
+# loop through assay CSVs and load them into the database
+assaypaths <- getAssayPaths(file.path(bioassayMirror, "Data"))
+for(assaypath in assaypaths){
+    aid <- as.integer(gsub("^.*?(\\d+)\\.concise\\.csv.*$", "\\1", assaypath, perl = TRUE))
+    print(paste("inserting activity for assay", aid))
+    tempAssay <- read.csv(assaypath)
+    tempAssay <- tempAssay[,c(1, 3, 4, 5)]
+    tempAssay <- cbind(PUBCHEM_AID=aid, tempAssay)
+    sql <- "INSERT INTO activity VALUES ($PUBCHEM_AID, $PUBCHEM_SID, $PUBCHEM_CID, $PUBCHEM_ACTIVITY_OUTCOME, $PUBCHEM_ACTIVITY_SCORE)"
+    dbBeginTransaction(con)
+    dbGetPreparedQuery(con, sql, bind.data = tempAssay)
+    dbCommit(con)
+}
+
+# parse assay descriptions from XML files
+assaypaths <- getAssayPaths(file.path(bioassayMirror, "Description"))
+parsedTable <- foreach(assaypath=assaypaths, .combine='rbind') %dopar% {
+    library(XML)
+    aid <- as.integer(gsub("^.*?(\\d+)\\.concise.descr\\.xml.*$", "\\1", assaypath, perl = TRUE))
+    print(paste("parsing XML for assay", aid))
+    desc <- xmlTreeParse(assaypath)
+    target <- desc[[1]][["PC-AssayContainer"]][["PC-AssaySubmit"]][["PC-AssaySubmit_assay"]][["PC-AssaySubmit_assay_descr"]][["PC-AssayDescription"]][["PC-AssayDescription_target"]][["PC-AssayTargetInfo"]][["PC-AssayTargetInfo_mol-id"]]
+    if(! is.null(target)){
+        target <- xmlToList(target)[[1]]
+    } else {
+        target <- NA
+    }
+    targetType <- desc[[1]][["PC-AssayContainer"]][["PC-AssaySubmit"]][["PC-AssaySubmit_assay"]][["PC-AssaySubmit_assay_descr"]][["PC-AssayDescription"]][["PC-AssayDescription_target"]][["PC-AssayTargetInfo"]][["PC-AssayTargetInfo_molecule-type"]]
+    if(! is.null(targetType)){
+        targetType <- xmlToList(targetType)$.attrs[["value"]]
+    } else if(! is.na(target)){
+        targetType <- "protein"
+    } else {
+        targetType <- NA
+    }
+    assayType <- desc[[1]][["PC-AssayContainer"]][["PC-AssaySubmit"]][["PC-AssaySubmit_assay"]][["PC-AssaySubmit_assay_descr"]][["PC-AssayDescription"]][["PC-AssayDescription_activity-outcome-method"]]
+    if(! is.null(assayType)){
+        assayType <- xmlToList(assayType)$.attrs[["value"]]
+    } else {
+        assayType <- NA
+    }
+    organism <- desc[[1]][["PC-AssayContainer"]][["PC-AssaySubmit"]][["PC-AssaySubmit_assay"]][["PC-AssaySubmit_assay_descr"]][["PC-AssayDescription"]][["PC-AssayDescription_target"]][["PC-AssayTargetInfo"]][["PC-AssayTargetInfo_organism"]][["BioSource"]][["BioSource_org"]][["Org-ref"]][["Org-ref_orgname"]][["OrgName"]][["OrgName_name"]][["OrgName_name_binomial"]][["BinomialOrgName"]][["BinomialOrgName_genus"]]  
+    if(! is.null(organism)){
+        organism <- xmlToList(organism)[[1]]
+    } else {
+        organism <- NA
+    }
+    c(aid, target, targetType, assayType, organism)
+}
+
+# insert assay data into database
+colnames(parsedTable) <- c("AID", "TARGETS", "TARGET_TYPE", "ASSAY_TYPE", "ORGANISM")
+parsedTable <- as.data.frame(parsedTable)
+sql <- "INSERT INTO assays VALUES (1, $AID, $TARGETS, $TARGET_TYPE, $ASSAY_TYPE, $ORGANISM)"
+dbBeginTransaction(con)
+dbGetPreparedQuery(con, sql, bind.data = parsedTable)
+dbCommit(con)
+
+# example use query
+dbGetQuery(con, "SELECT * FROM activity LIMIT 10")
+
+# disconnect:
+dbDisconnect(con)
