7ea6763ad255ef3da7dc79082358f5a3c9fd7a84
Tyler Backman <tbackman@ucr.edu>
2013-06-24 11:47:18 -0700

    minor changes

--- scripts/addAssayDetails.R
+++ scripts/addAssayDetails.R
@@ -17,7 +17,7 @@ outputDatabase = commandArgs(trailingOnly=TRUE)[2]
 
 # test code for running without make:
 if(is.na(commandArgs(trailingOnly=TRUE)[1])){
-	bioassayMirror <- "working/bioassayMirror"
+	bioassayMirror <- "working/bioassayMirror_test"
 	outputDatabase <- "working/bioassayDatabaseWithAssayDetails.sqlite"
 }
 
@@ -41,21 +41,33 @@ con <- dbConnect(drv, dbname=outputDatabase)
 dataAids <- dbGetQuery(con, "SELECT DISTINCT aid FROM activity")[,1]
 assaypaths <- assaypaths[aids %in% dataAids]
 
-# get target molecules
-targetTable <- foreach(assaypath=assaypaths, .combine='rbind') %dopar% {
+# parse assay annotation details 
+resultsTable <- foreach(assaypath=assaypaths, .combine='rbind') %dopar% {
     aid <- as.integer(gsub("^.*?(\\d+)\\.concise.descr\\.xml.*$", "\\1", assaypath, perl = TRUE))
     xmlPointer <- xmlTreeParse(assaypath, useInternalNodes=TRUE, addFinalizer=TRUE)
     targets <- xpathSApply(xmlPointer, "//x:PC-AssayTargetInfo_mol-id/text()", xmlValue, namespaces="x")
     targetTypes <- xpathSApply(xmlPointer,"//x:PC-AssayTargetInfo_molecule-type/@value", namespaces="x")
+    type <- xpathSApply(xmlPointer, "//x:PC-AssayDescription_activity-outcome-method/@value", namespaces="x")[[1]]
     free(xmlPointer)
-    if(is.null(targets)){
-	return(NULL)
-    } else {
-        return(cbind(aid, targets, targetTypes))
+    if(is.null(type)){
+	type <- NA
     }
+    if(is.null(targets)){
+        targets <- NA	
+    } 
+    if(is.null(targetTypes)){
+        targetTypes <- NA	
+    } 
+    if(length(targets) != length(targetTypes)){
+    	stop(paste("error with aid", aid))
+    }
+    targetTypes <- targetTypes[! duplicated(targets)]
+    targets <- targets[! duplicated(targets)]
+    return(cbind(aid, targets, targetTypes, type))
 }
 
 # load target molecules
+targetTable <- resultsTable[! is.na(resultsTable[,2]),1:3]
 dbGetQuery(con, "CREATE TABLE targets (aid INTEGER, target TEXT, target_type TEXT)")
 colnames(targetTable) <- c("AID", "TARGET", "TARGET_TYPE")
 targetTable <- as.data.frame(targetTable)
@@ -64,25 +76,14 @@ dbBeginTransaction(con)
 dbGetPreparedQuery(con, sql, bind.data = targetTable)
 dbCommit(con)
 
-# get other assay details
-parsedTable <- foreach(assaypath=assaypaths, .combine='rbind') %dopar% {
-    aid <- as.integer(gsub("^.*?(\\d+)\\.concise.descr\\.xml.*$", "\\1", assaypath, perl = TRUE))
-    xmlPointer <- xmlTreeParse(assaypath, useInternalNodes=TRUE, addFinalizer=TRUE)
-    type <- xpathSApply(xmlPointer, "//x:PC-AssayDescription_activity-outcome-method/@value", namespaces="x")[[1]]
-    if(is.null(type)){
-	type <- NA
-    }
-    free(xmlPointer)
-    cbind(aid, type)
-}
-
 # load other assay details
+assayTable <- resultsTable[! duplicated(resultsTable[,1]),c(1,4)] 
 dbGetQuery(con, "CREATE TABLE assays (source_id INTEGER, aid INTEGER, assay_type TEXT)")
-colnames(parsedTable) <- c("AID", "ASSAY_TYPE")
-parsedTable <- as.data.frame(parsedTable)
+colnames(assayTable) <- c("AID", "ASSAY_TYPE")
+assayTable <- as.data.frame(assayTable)
 sql <- "INSERT INTO assays VALUES (1, $AID, $ASSAY_TYPE)"
 dbBeginTransaction(con)
-dbGetPreparedQuery(con, sql, bind.data = parsedTable)
+dbGetPreparedQuery(con, sql, bind.data = assayTable)
 dbCommit(con)
 
 dbDisconnect(con)
