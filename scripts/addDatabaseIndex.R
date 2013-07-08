6fe702a40db4613ec49f097b36b0c11cd06691be
Tyler Backman <tbackman@ucr.edu>
2013-07-08 10:09:44 -0700

    building new index defs

--- scripts/addDatabaseIndex.R
+++ scripts/addDatabaseIndex.R
@@ -8,18 +8,27 @@ library(RSQLite)
 
 outputDatabase = commandArgs(trailingOnly=TRUE)[1]
 
+# test code for running without make:
+if(is.na(commandArgs(trailingOnly=TRUE)[1])){
+    outputDatabase <- "working/indexedBioassayDatabase.sqlite"
+}
+
 drv <- dbDriver("SQLite")
 con <- dbConnect(drv, dbname=outputDatabase)
 
 # add indexes
-dbGetQuery(con, "CREATE UNIQUE INDEX IF NOT EXISTS assays_aid ON assays (aid)")
+dbGetQuery(con, "CREATE INDEX IF NOT EXISTS activity_cid ON activity (cid)")
+dbGetQuery(con, "CREATE INDEX IF NOT EXISTS targets_aid ON targets (aid)")
+
+# old indicies (rethink)
+# dbGetQuery(con, "CREATE UNIQUE INDEX IF NOT EXISTS assays_aid ON assays (aid)")
 # dbGetQuery(con, "CREATE INDEX IF NOT EXISTS assays_targets ON assays (targets)")
 # dbGetQuery(con, "CREATE INDEX IF NOT EXISTS assays_target_type ON assays (target_type)")
-dbGetQuery(con, "CREATE INDEX IF NOT EXISTS assays_assay_type ON assays (assay_type)")
-dbGetQuery(con, "CREATE INDEX IF NOT EXISTS activity_aid ON activity (aid)")
-dbGetQuery(con, "CREATE INDEX IF NOT EXISTS activity_cid ON activity (cid)")
-dbGetQuery(con, "CREATE INDEX IF NOT EXISTS activity_activity ON activity (activity)")
-dbGetQuery(con, "CREATE INDEX IF NOT EXISTS domainIndex ON domains (domain)")
-dbGetQuery(con, "CREATE INDEX IF NOT EXISTS targetIndex ON domains (target)")
+# dbGetQuery(con, "CREATE INDEX IF NOT EXISTS assays_assay_type ON assays (assay_type)")
+# dbGetQuery(con, "CREATE INDEX IF NOT EXISTS activity_aid ON activity (aid)")
+# dbGetQuery(con, "CREATE INDEX IF NOT EXISTS activity_cid ON activity (cid)")
+# dbGetQuery(con, "CREATE INDEX IF NOT EXISTS activity_activity ON activity (activity)")
+# dbGetQuery(con, "CREATE INDEX IF NOT EXISTS domainIndex ON domains (domain)")
+# dbGetQuery(con, "CREATE INDEX IF NOT EXISTS targetIndex ON domains (target)")
 
 dbDisconnect(con)
