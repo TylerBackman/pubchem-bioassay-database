f71e593d727187acca2be79aa230970fa2cb71aa
Tyler Backman <tbackman@ucr.edu>
2014-09-09 15:07:27 -0700

    got mpi jobs working

--- src/computeAtomPairs.R
+++ src/computeAtomPairs.R
@@ -8,8 +8,6 @@ library(R.utils)
 library(ChemmineR)
 library(bioassayR)
 library(foreach)
-library(modules) # if you don't have this, run install.packages("/home_girkelab/tbackman/Projects/modules4R/modules_1.0.tar.gz", repos=NULL)
-moduleload("openmpi")
 library(snow)
 library(doSNOW)
 
@@ -41,13 +39,20 @@ sdfs <- sdfs[grep(".sdf.gz$", sdfs)]
 cl <- makeCluster(nodes, type = "MPI") 
 registerDoSNOW(cl)
 
+# combine function to drop "FALSE" results
+combineAtomPairs <- function(ap1, ap2){
+    if(! is.logical(ap2)) return(c(ap1, ap2))
+    return(ap1)
+}
+
 # compute atom pairs in parallel on cluster
-results <- foreach(i = sdfs, .combine='c') %dopar% {
+results <- foreach(i = sdfs, .combine='combineAtomPairs', .inorder=TRUE) %dopar% {
     lowerLimit <- as.numeric(gsub("^.*Compound_(\\d+)_(\\d+).sdf.gz", "\\1", i))
     higherLimit <- as.numeric(gsub("^.*Compound_(\\d+)_(\\d+).sdf.gz", "\\2", i))
     cidSubset <- cids[cids <= higherLimit]
     cidSubset <- cidSubset[cidSubset >= lowerLimit]
     if(length(cidSubset) == 0) return(FALSE)    
+    library(ChemmineR)
     mySdf <- read.SDFset(i)
     mySdf <- mySdf[validSDF(mySdf)]
     if(length(mySdf) == 0) return(FALSE)    
@@ -55,8 +60,10 @@ results <- foreach(i = sdfs, .combine='c') %dopar% {
     if(length(mySdf) == 0) return(FALSE)    
     ap <- sdf2ap(mySdf)
     cid(ap) <- sdfid(mySdf)
-    return(ap)
+    return(as(ap, "list"))
 }
 
+# save results and quit
 stopCluster(cl)
-save(list = ls(all=TRUE), file = outfile)
+results <- as(results, "APset")
+save(list = c("results"), file = outfile)
