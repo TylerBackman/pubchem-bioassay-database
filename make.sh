67df7d0e7de496c20e6194e6134ad4c074f10e37
Tyler Backman <tbackman@ucr.edu>
2015-12-02 15:07:22 -0800

    updated make.sh file

--- make.sh
+++ make.sh
@@ -3,10 +3,11 @@
 #PBS -j oe
 #PBS -l walltime=48:00:00
 #PBS -l nodes=1:ppn=2
-#PBS -l mem=256gb
+#PBS -l mem=64gb
 ##PBS -q highmem
 
 cd $PBS_O_WORKDIR
 
 export mpiCores="$PBS_NP"
+module load R/3.2.2 
 make -e working/summarystats.txt
