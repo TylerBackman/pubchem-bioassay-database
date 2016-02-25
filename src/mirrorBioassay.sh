3e5186b6289bf3461940598e369c107e2e438ca3
Tyler Backman <tbackman@ucr.edu>
2016-02-25 10:20:29 -0800

    moved source from concise to full

--- src/mirrorBioassay.sh
+++ src/mirrorBioassay.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Purpose: makes a mirror of pubchem bioassay in the specified folder 
-# (C) 2013 Tyler WH Backman
+# (C) Tyler WH Backman
 
 cd $1
-wget ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/Concise/CSV/README
+wget ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/CSV/README
 
 mkdir Description
 cd Description
-wget -r -nd ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/Concise/CSV/Description/
+wget -r -nd ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/CSV/Description/
 cd ..
 
 mkdir Data
 cd Data
-wget -r -nd ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/Concise/CSV/Data/
+wget -r -nd ftp://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/CSV/Data/
 unzip "*.zip"
 rm -f *.zip
 
