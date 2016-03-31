e4ccc36c63403334a98c817c1e851277c57145fc
Tyler Backman <tbackman@ucr.edu>
2016-03-31 11:23:48 -0700

    updated makefile

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 #PBS -j oe
 #PBS -l walltime=48:00:00
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=8
 #PBS -l mem=8gb
 ##PBS -q highmem
 
@@ -11,4 +11,4 @@ cd $PBS_O_WORKDIR
 export cores="$PBS_NP"
 module load R/3.2.2 
 module load hmmer/3.1b2
-make -e working/scoreCategories.txt
+make -e working/summarystats.txt
