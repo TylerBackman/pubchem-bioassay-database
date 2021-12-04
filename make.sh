1bbe9a820b76296f80118f048dbe3c57eadb910e
dcassol <danicassol@gmail.com>
2021-12-04 12:01:32 -0800

    make

--- make.sh
+++ make.sh
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=10G
 #SBATCH --time=1-00:15:00     # 1 day and 15 minutes
-#SBATCH --mail-user=danicassol@gmail.com
+#SBATCH --mail-user=username@email.com
 #SBATCH --mail-type=ALL
 #SBATCH --job-name="database"
-#SBATCH -p girkelab # This is the default partition, you can use any of the following; intel, batch, highmem, gpu
+#SBATCH -p intel # This is the default partition, you can use any of the following; intel, batch, highmem, gpu
 
 cd $PBS_O_WORKDIR
 
